2360d8e4454b2d36ee95fabb2890578359d4fbf6
lovebird <lovebirdsx@gmail.com>
2023-04-14 12:52:05 +0800

    优化体验和代码

--- src/code_explainer.py
+++ src/code_explainer.py
@@ -1,21 +1,26 @@
-from common import Prompt, run_app_by_prompt
+import argparse
+
+from cache import Cache
+from asker import Prompt, do_ask_for_large_file_cmd
+from common import open_file
 
 TRUNK_PROMPT_FIRST = '''The code I sent you is a part of a code file.
-Please help me generate a summary.
-After all the summaries are generated, I will send them to you together for the final summary.
-Please make sure the summaries you generate are easy for you to perform the final summary:'''
+Please help me generate a summary. include the most unique and helpful points.
+no need to include all the details. 300 words or less. Reply in chinese:'''
 
 TRUNK_PROMPT_NEXT = '''The following code is a follow-up to the code I sent you before,
-and all of them come from the same file.
-Please continue to generate summary:'''
+Please help me generate a summary. include the most unique and helpful points.
+no need to include all the details. 300 words or less. Reply in chinese:'''
 
 SUMARIZE_MUTI_PROMPT = '''The following content is a summary of different parts of the same code file.
 Please summarize them with the most unique and helpful points, into a list of key points and takeaways.
-Please reply in chinese:'''
+Reply in chinese:'''
 
 SUMARIZE_SINGLE_PROMPT = '''The texts I send to you are all come form the same code file,
 Summarize them with the most unique and helpful points,
 into a list of key points and takeaways. Reply in chinese:'''
+
+PROG_NAME = 'code_explainer'
 
 def create_prompt() -> Prompt:
     return Prompt(
@@ -25,5 +30,53 @@ def create_prompt() -> Prompt:
         SUMARIZE_SINGLE_PROMPT
     )
 
+def create_args_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description=PROG_NAME)
+    parser.add_argument('-t', '--test', action='store_true', help='是否为测试模式')
+    parser.add_argument('-c', '--cache', action='store_true', help='是否使用缓存')
+    parser.add_argument('-f', '--file', help='代码文件路径')
+
+    return parser
+
+def gen_explain_header(path: str) -> str:
+    lines = [
+        f'# {path}',
+        '',
+        f'[打开](file:///{path})'
+        '',
+        '',
+    ]
+    return '\n'.join(lines)
+
+def test() -> None:
+    print('测试通过')
+
 if __name__ == '__main__':
-    run_app_by_prompt(create_prompt(), 'code_explainer')
+    parser = create_args_parser()
+    args = parser.parse_args()
+
+    if args.test:
+        test()
+        exit(0)
+
+    if not args.file:
+        print(parser.format_help())
+        exit(0)
+    
+    cache = Cache(PROG_NAME)
+    if args.cache:
+        result = cache.read(args.file)
+        if result:
+            print(result)
+            open_file(cache.get_cache_path(args.file))
+            exit(0)
+
+    prompt = create_prompt()
+    if args.file:
+        result = do_ask_for_large_file_cmd(args.file, prompt)
+        if not result:
+            exit(1)
+
+        cache.write(args.file, gen_explain_header(args.file) + '\n' + result + '\n')
+        open_file(cache.get_cache_path(args.file))
+        exit(0)
--- src/code_reviewer.py
+++ src/code_reviewer.py
@@ -1,11 +1,12 @@
-import os, sys
+import argparse
+import os
 import git
-from common import Prompt, get_save_path, run_app_by_prompt
+from app import get_save_path
+from asker import Prompt, do_ask_for_large_file_cmd
+from common import open_file, write_file
 
 TRUNK_PROMPT_FIRST = '''The content I sent you is a part of a code patch.
 please help me do a brief code review. If any bug risk and improvement suggestion are welcome.
-After all the code review are generated, I will send them to you together for the final summary.
-Please make sure the reviews you generate are easy for you to perform the final review.
 Reply in chinese:'''
 
 TRUNK_PROMPT_NEXT = '''Please continue to do code view.
@@ -18,6 +19,8 @@ Reply in chinese:'''
 SUMARIZE_SINGLE_PROMPT = '''Bellow is the code patch, please help me do a brief code review.
 If any bug risk and improvement suggestion are welcome. Reply in chinese:'''
 
+PROG_NAME = 'code_reviewer'
+
 def create_prompt() -> Prompt:
     return Prompt(
         TRUNK_PROMPT_FIRST,
@@ -26,16 +29,16 @@ def create_prompt() -> Prompt:
         SUMARIZE_SINGLE_PROMPT
     )
 
-if __name__ == '__main__':
-    file = sys.argv[1]
-    if not os.path.exists(file):
-        print(f'文件{file}不存在')
-        exit(1)
+def create_args_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description=PROG_NAME)
+    parser.add_argument('-t', '--test', action='store_true', help='是否为测试模式')
+    parser.add_argument('-f', '--file', help='文件路径，将以此文件所在目录为git仓库根目录')
 
-    repo = git.Repo(os.path.dirname(file), search_parent_directories=True)
+    return parser
+
+def gen_patch(repo: git.Repo, file) -> str:
     if not repo:
-        print(f'文件[{file}]所在目录不是git仓库')
-        exit(1)
+        raise Exception(f'文件[{file}]所在目录不是git仓库')
     
     repo_name = os.path.basename(repo.working_dir)
     patch_path = os.path.join(get_save_path(), f'{repo_name}.patch')
@@ -48,8 +51,59 @@ if __name__ == '__main__':
         os.system(f'cd {os.path.dirname(file)} && git format-patch -1 --stdout > {patch_path}')
 
     if not os.path.exists(patch_path):
-        print(f'无法生成patch文件, 请检查文件[{file}]所在目录是否为git仓库')
-        exit(1)
+        raise Exception(f'无法生成patch文件, 请检查文件[{file}]所在目录是否为git仓库')
+    return patch_path
 
+def gen_review_header(repo: git.Repo) -> str:
+    repo_name = os.path.basename(repo.working_dir)
+    branch_name = repo.active_branch.name
+    commit_id = repo.head.commit.hexsha
+    commit_message = repo.head.commit.message
+
+    if not repo.is_dirty():
+        lines = [
+            f'# 版本库：{repo_name} 分支：{branch_name} 提交：{commit_id}',
+            '',
+            f'提交信息 {commit_message} by {repo.head.commit.author}',
+            '',
+        ]
+    else:
+        lines = [
+            f'# 版本库：{repo_name} 分支：{branch_name} (未提交)',
+            '',
+        ]
+    return '\r\n'.join(lines)
+    
+
+def test() -> None:
+    repo = git.Repo(os.path.dirname(__file__), search_parent_directories=True)
+    print(gen_review_header(repo))
+
+
+if __name__ == '__main__':
+    parser = create_args_parser()
+    args = parser.parse_args()
+
+    if args.test:
+        test()
+        exit(0)
+
+    file = args.file
+    if not file:
+        print(parser.format_help())
+        exit(0)
+
+    repo = git.Repo(os.path.dirname(file), search_parent_directories=True)
+    patch_path = gen_patch(repo, file)
     print(f'生成patch文件[{patch_path}]')
-    run_app_by_prompt(create_prompt(), 'code_reviewer', args=['-f', patch_path])
+
+    prompt = create_prompt()
+    result = do_ask_for_large_file_cmd(patch_path, prompt)
+    
+    review_header = gen_review_header(repo)
+    repo_name = os.path.basename(repo.working_dir)
+    result_path = os.path.normpath(os.path.join(get_save_path(), f'code_reviewer/{repo_name}.md'))
+    write_file(result_path, review_header + result)
+    print(f'结果保存在：{result_path}')
+    open_file(result_path)
+        
